63fe71548638f0427f4f07efc24db9e96237ac81
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-01-20 18:30:38 -0300

    Add button to make a new grid

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".game-container");
+const btn = document.querySelector("button");
 
-for (let i = 0; i < 256; i++) {
-  createSquare();
-  console.log(`Square ${i} created!`);
-}
+createGrid(16);
+
+btn.addEventListener("click", () => {
+  const input = prompt("Enter a number from 1 to 100:");
+  if (isNaturalNumberBetween1And100(input)) {
+    createGrid(input);
+  } else {
+    alert("Please insert a valid number.");
+  }
+  return;
+})
 
 function createSquare() {
   const square = document.createElement("div");
@@ -13,3 +21,19 @@ function createSquare() {
   container.appendChild(square);
   return;
 }
+
+function createGrid (number) {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastElementChild);
+  }
+  number = number * number;
+  for (let i = 0; i < number; i++) {
+    createSquare();
+  }
+  return;
+}
+
+function isNaturalNumberBetween1And100 (num) {
+  return Number.isInteger(num) && num >= 1 && num <= 100;
+}
+
