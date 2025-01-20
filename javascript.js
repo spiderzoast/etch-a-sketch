f6c065946ee54f0ea1c736d0a2b4a798346013fd
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-01-20 19:27:34 -0300

    Add logic for the new grid button

--- javascript.js
+++ javascript.js
@@ -1,34 +1,46 @@
-const container = document.querySelector(".game-container");
+const gameContainer = document.querySelector(".game-container");
 const btn = document.querySelector("button");
 
 createGrid(16);
 
 btn.addEventListener("click", () => {
   const input = prompt("Enter a number from 1 to 100:");
-  if (isNaturalNumberBetween1And100(input)) {
+  if (input === null) {
+    return;
+  }
+  if (isNaturalNumberBetween1And100(Number(input))) {
+    console.log("Grid being created!");
     createGrid(input);
+    return;
   } else {
     alert("Please insert a valid number.");
+    return;
   }
-  return;
-})
+});
 
-function createSquare() {
-  const square = document.createElement("div");
-  square.addEventListener("mouseover", () => {
-    square.setAttribute("class", "hovered-square");
+// 800 refers to width and height of the game-container,
+// this calculation is required to make any grid size 
+// from 1 to 100 occupy the same 800x800 pixels.
+function createSquare(gridSize) {
+  const pixelSize = 800 / gridSize;
+  const newCell = document.createElement("div");
+  newCell.classList.add("cell");
+  newCell.setAttribute("style", `width: ${pixelSize}px; height: ${pixelSize}px`);
+  newCell.addEventListener("mouseover", () => {
+    newCell.classList.add("cell-hovered");
   });
-  container.appendChild(square);
+  gameContainer.appendChild(newCell);
   return;
 }
 
-function createGrid (number) {
-  while (container.hasChildNodes()) {
-    container.removeChild(container.lastElementChild);
+function createGrid (gridSize) {
+  while (gameContainer.hasChildNodes()) {
+    gameContainer.removeChild(gameContainer.lastElementChild);
   }
-  number = number * number;
-  for (let i = 0; i < number; i++) {
-    createSquare();
+  const totalCells = gridSize ** 2;
+  for (let i = 0; i < totalCells; i++) {
+    console.log(`Cell #${(i+1)} created.`)
+    createSquare(gridSize);
   }
   return;
 }
