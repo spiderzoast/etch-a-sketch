db915e62571b0cd2f17962ea0f3aecdad8d8459a
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-01-20 17:11:16 -0300

    Change square color when hovered

--- javascript.js
+++ javascript.js
@@ -7,6 +7,9 @@ for (let i = 0; i < 256; i++) {
 
 function createSquare() {
   const square = document.createElement("div");
+  square.addEventListener("mouseover", () => {
+    square.setAttribute("class", "hovered-square");
+  });
   container.appendChild(square);
   return;
 }
